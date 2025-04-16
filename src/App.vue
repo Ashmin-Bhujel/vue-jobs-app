13a9d146688a16f0f2ba7c0874718e59dc2ed08f
Ashmin Bhujel <ashminbhujel01@gmail.com>
2025-04-16 23:37:36 +0545

    add: Dark and light color mode

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <UApp>
     <h1>Vue Jobs App</h1>
-    <UButton icon="fa6-brands:github">Hello</UButton>
+    <UButton icon="fa6-brands:github" color="neutral">Hello</UButton>
+    <ColorModeButton />
   </UApp>
 </template>
